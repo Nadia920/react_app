5307467c9eccf56f41ce68e94ffe56755c15a96b
Nadia920 <kabaknada64@gmail.com>
2022-07-27 23:36:39 +0300

    Added buttons add date in the list in the page "Notes.js"

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,9 +3,15 @@ export const Notes = ({notes}) => {
     return (
         <ul className="list-group">
             {notes.map(note => (
-                <li className="list-group-item"
+                <li className="list-group-item note"
                 key={note.id}
-                >{note.title}</li>
+                >
+                    <strong>{note.title}</strong>
+                    <span>{new Date().toLocaleDateString()}</span>
+                <button
+                type="button" className="btn btn-outline-danger btn-sm">&times;</button>
+                </li>
+
             ))}
 
         </ul>
