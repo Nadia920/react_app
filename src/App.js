5d998631adcdb6809dc430211d7d2d61f80bf6b6
Nadia920 <kabaknada64@gmail.com>
2022-07-28 00:02:42 +0300

    Added a file "Alert.js" that is needed to display information when editing our entries in notes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import {Home} from './pages/Home'
 import {About} from "./pages/About";
 import {Navbar} from "./components/Navbar";
+import {Alert} from "./components/Alert";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
       <BrowserRouter>
           <Navbar />
       <div className="container pt-4">
+          <Alert />
       <Routes>
         <Route path={'/'} element={<Home />} />
           <Route path={'/about'} element={<About />} />
