84dd77e01c09fb78575ea8737e0f65d7d70b603a
Nadia920 <kabaknada64@gmail.com>
2022-07-28 12:14:01 +0300

    Feat. Finish writing the code for popup messages and getting them ready for use

--- src/context/alert/alertReducer.js
+++ src/context/alert/alertReducer.js
@@ -1,5 +1,9 @@
+import {HIDE_ALERT, SHOW_ALERT} from "../types";
+
 const handlers = {
-   DEFAULT: state => state
+    [SHOW_ALERT]: (state, {payload}) => ({...payload, visible: true}),
+    [HIDE_ALERT]: state => ({...state, visible: false}),
+    DEFAULT: state => state
 }
 export const alertReducer = (state, action) => {
     const handle = handlers[action.type] || handlers.DEFAULT
