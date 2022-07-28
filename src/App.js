52090376c9041c77641e1a55abddcbf158927066
Nadia920 <kabaknada64@gmail.com>
2022-07-28 14:17:29 +0300

    Feat. Notepad entry states are set

--- src/App.js
+++ src/App.js
@@ -8,25 +8,11 @@ import {About} from "./pages/About";
 import {Navbar} from "./components/Navbar";
 import {Alert} from "./components/Alert";
 import {AlertState} from "./context/alert/AlertState.s";
+import {FirebaseState} from "./context/firebase/FirebaseState";
 
 function App() {
     return (
-        /*<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>*/
+        <FirebaseState>
         <AlertState>
             <BrowserRouter>
                 <Navbar/>
@@ -39,6 +25,7 @@ function App() {
                 </div>
             </BrowserRouter>
         </AlertState>
+        </FirebaseState>
     );
 }
 
