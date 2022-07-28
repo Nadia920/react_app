7ac74fdfbd5fc3efa29cafc03ab565b15d4dd999
Nadia920 <kabaknada64@gmail.com>
2022-07-28 11:42:05 +0300

    Feat. Set states for popup messages

---
+++ src/context/alert/alertReducer.js
@@ -0,0 +1,7 @@
+const handlers = {
+   DEFAULT: state => state
+}
+export const alertReducer = (state, action) => {
+    const handle = handlers[action.type] || handlers.DEFAULT
+   return handle(state, action)
+}
