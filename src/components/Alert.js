742be9acb49468d52366e2e8ad0da9370cf30af9
Nadia920 <kabaknada64@gmail.com>
2022-07-28 12:39:33 +0300

    Feat. Displaying two types of pop-up messages

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,15 @@
-import React from 'react'
-export const Alert = ({alert}) => {
-    if (!alert) {
+import React, {useContext} from 'react'
+import {AlertContext} from "../context/alert/alertContext";
+export const Alert = () => {
+    const {alert, hide} = useContext(AlertContext)
+    if (!alert.visible) {
         return null
     }
     return (
         <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} >
             <strong>Внимание!</strong>
             {alert.text}
-            <button type="button" className="btn-close"  aria-label="Close"></button>
+            <button onClick={hide} type="button" className="btn-close"  aria-label="Close"></button>
         </div>
     )
 }
