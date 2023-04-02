c9975d3875a95c3b8380e872a6e364b4aa967ea9
Nadia920 <kabaknada64@gmail.com>
2023-04-02 20:51:16 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-/*import logo from './logo.svg';*/
-/*import './App.css';*/
-import React from 'react'
-/*import {BrowserRouter, Routes, Route} from 'react-router-dom'*/
-import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {Home} from './pages/Home'
-import {About} from "./pages/About";
-import {Navbar} from "./components/Navbar";
-import {Alert} from "./components/Alert";
-import {AlertState} from "./context/alert/AlertState.s";
-import {FirebaseState} from "./context/firebase/FirebaseState";
+import {About} from './pages/About'
+import {Navbar} from './components/Navbar'
+import {Alert} from './components/Alert'
+import {AlertState} from './context/alert/AlertState'
+import {FirebaseState} from './context/firebase/FirebaseState'
 
 function App() {
-    return (
-        <FirebaseState>
-        <AlertState>
-            <BrowserRouter>
-                <Navbar/>
-                <div className="container pt-4">
-                    <Alert/>
-                    <Routes>
-                        <Route path={'/'} element={<Home/>}/>
-                        <Route path={'/about'} element={<About/>}/>
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </AlertState>
-        </FirebaseState>
-    );
+  return (
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar/>
+          <div className="container pt-4">
+            <Alert/>
+            <Switch>
+              <Route path={'/'} exact component={Home}/>
+              <Route path={'/about'} component={About}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
+  );
 }
 
 export default App;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react'
-export const About = () => {
-    return (
-        <Fragment>
-            <h1>About page</h1>
-        </Fragment>
-    )
-}
+import React from 'react'
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Приложения о заметках</h1>
+    </div>
+  </div>
+)
